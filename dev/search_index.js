8a8db4147bee4c7af23f735187c5a75279169ce5
Documenter.jl <documenter@juliadocs.github.io>
2023-10-23 12:08:49 +0000

    build based on 055162d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastTanhSinhQuadrature","category":"page"},{"location":"#FastTanhSinhQuadrature","page":"Home","title":"FastTanhSinhQuadrature","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastTanhSinhQuadrature.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastTanhSinhQuadrature]","category":"page"}]
+}
