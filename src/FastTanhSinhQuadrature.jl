772e7bb84a0ccf0d2df2b335d77cb09d6d5dd12a
Stamatis Vretinaris <svretina@gmail.com>
2024-01-11 17:42:43 +0200

    add AVX instructions to loops

--- src/FastTanhSinhQuadrature.jl
+++ src/FastTanhSinhQuadrature.jl
@@ -2,18 +2,20 @@ module FastTanhSinhQuadrature
 
 using StaticArrays
 using LambertW
+using LoopVectorization
 
 export tanhsinh, integrate, quad
 
 @inline function ordinate(t::T) where {T<:Real}
-    return tanh(T(π) / 2 * sinh(t))
+    return @fastmath tanh(T(π) / 2 * sinh(t))
 end
 @inline function weight(t::T) where {T<:Real}
-    return ((T(π) / 2) * cosh(t)) / cosh(T(π) / 2 * sinh(t))^2
+    tmp = cosh(T(π) / 2 * sinh(t))
+    return @fastmath ((T(π) / 2) * cosh(t)) / (tmp * tmp)
 end
 
 @inline function inv_ordinate(t::T) where {T<:Real}
-    return asinh(log((one(T) + t) / (one(T) - t)) / T(π))
+    return @fastmath asinh(log((one(T) + t) / (one(T) - t)) / T(π))
 end
 
 function tanhsinh(::Type{T}, n::Int) where {T<:AbstractFloat}
@@ -22,7 +24,11 @@ function tanhsinh(::Type{T}, n::Int) where {T<:AbstractFloat}
     t = h:h:tmax
     x = ordinate.(t)
     w = weight.(t)
-    return x, w, h
+    if n < 100
+        return SVector{n,T}(x), SVector{n,T}(w), h
+    else
+        return x, w, h
+    end
 end
 
 tanhsinh(n::Int) = tanhsinh(Float64, n)
@@ -55,6 +61,47 @@ function tanhsinh!(::Type{T}, x::AbstractVector{T}, w::AbstractVector{T},
     return nothing
 end
 
+@inline function remove_endpoints!(xmin::T, xmax::T, x, w) where {T}
+    Δx = (xmax - xmin) / 2
+    x₀ = (xmax + xmin) / 2
+    @fastmath @inbounds for i in 1:length(x)
+        Δxxi = Δx * x[i]
+        xp = x₀ + Δxxi
+        xm = x₀ - Δxxi
+        if xp ≥ xmax || xm ≤ xmin
+            x[i] = x₀
+            w[i] = zero(T)
+        end
+    end
+end
+
+@inline function remove_left_endpoint!(xmin::T, xmax::T, x, w) where {T}
+    Δx = (xmax - xmin) / 2
+    x₀ = (xmax + xmin) / 2
+    @fastmath @inbounds for i in 1:length(x)
+        Δxxi = Δx * x[i]
+        xp = x₀ + Δxxi
+        xm = x₀ - Δxxi
+        if xm ≤ xmin
+            x[i] = x₀
+            w[i] = zero(T)
+        end
+    end
+end
+
+@inline function remove_right_endpoint!(xmin::T, xmax::T, x, w) where {T}
+    Δx = (xmax - xmin) / 2
+    x₀ = (xmax + xmin) / 2
+    @fastmath @inbounds for i in 1:length(x)
+        Δxxi = Δx * x[i]
+        xp = x₀ + Δxxi
+        xm = x₀ - Δxxi
+        if xp ≥ xmax
+            x[i] = x₀
+            w[i] = zero(T)
+        end
+    end
+end
 
 function integrate(::Type{T}, f::Function, n::Int) where {T<:Real}
     x, w, h = tanhsinh(T, n)
@@ -87,46 +134,61 @@ function integrate(f::Function, x::AbstractVector{T}, w::AbstractVector{T},
 end
 
 # [a,b] 1D
-function integrate(f::S, xmin::T, xmax::T, x::AbstractVector{T},
+@inline function integrate(f::S, xmin::T, xmax::T, x::AbstractVector{T},
     w::AbstractVector{T}, h::T) where {T<:Real,S}
-    Δx = (xmax - xmin) / 2
-    x₀ = (xmax + xmin) / 2
-    s = weight(zero(T)) * f(x₀)
-    #ncalls[1] += 1
-    for i in 1:length(x)
-        xp = x₀ + Δx * x[i]
-        xm = x₀ - Δx * x[i]
-        if xm > xmin
-            s += w[i] * f(xm)
-            #ncalls[1] += 1
-        end
-        if xp < xmax
-            s += w[i] * f(xp)
-            # ncalls[1] += 1
+    @fastmath @inbounds begin
+        Δx = (xmax - xmin) / 2
+        x₀ = (xmax + xmin) / 2
+        s = weight(zero(T)) * f(x₀)
+        #ncalls[1] += 1
+        for i in 1:length(x)
+            xp = x₀ + Δx * x[i]
+            xm = x₀ - Δx * x[i]
+            if xm > xmin
+                s += w[i] * f(xm)
+                #ncalls[1] += 1
+            end
+            if xp < xmax
+                s += w[i] * f(xp)
+                # ncalls[1] += 1
+            end
         end
     end
     return Δx * h * s
 end
 
-## 2D
-function integrate(f::Function, xmin::SVector{2,T}, xmax::SVector{2,T}, x::AbstractVector{T},
-    w::AbstractVector{T}, h::T) where {T<:Real}
-    function f1(x1::T) where {T<:Real}
-        g1(y::T) where {T} = f(x1, y)
-        res = integrate(g1, xmin[2], xmax[2], x, w, h)
-        return res
+
+## carefull, this is unsafe for a function with a singularity at the endpoints
+## if you want to use this with a singular function, then first run
+## remove_endpoints! on your weights and points and then use this function
+@inline function integrate_avx(f::S, xmin::T, xmax::T, x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real,S}
+    @fastmath Δx = (xmax - xmin) / 2
+    @fastmath x₀ = (xmax + xmin) / 2
+    @fastmath s = weight(zero(T)) * f(x₀)
+    @turbo for i in 1:length(x)
+        Δxxi = Δx * x[i]
+        xp = x₀ + Δxxi
+        xm = x₀ - Δxxi
+        s += w[i] * (f(xm) + f(xp))
     end
-    g2(x1::T) where {T} = f1(x1)
-    res = integrate(g2, xmin[1], xmax[1], x, w, h)
-    return res
+    return @fastmath Δx * h * s
+end
+
+## 2D
+@inline function integrate(f::S, xmin::SVector{2,T}, xmax::SVector{2,T},
+    x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real,S}
+    @inline function f2(x1::T) where {T<:Real}
+        return @inbounds integrate_avx(y -> f(x1, y), xmin[2], xmax[2], x, w, h)
+    end
+    return @inbounds integrate(x1 -> f2(x1), xmin[1], xmax[1], x, w, h)
 end
 
 ## 3D
-function integrate(f::S, xmin::SVector{3,T}, xmax::SVector{3,T},
+@inline function integrate(f::S, xmin::SVector{3,T}, xmax::SVector{3,T},
     x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real,S}
-    f1(x1::T, y1::T) where {T<:Real} = integrate(z -> f(x1, y1, z), xmin[3], xmax[3], x, w, h)
-    f2(x1::T) where {T<:Real} = integrate(y -> f1(x1, y), xmin[2], xmax[2], x, w, h)
-    return integrate(x1 -> f2(x1), xmin[1], xmax[1], x, w, h)
+    f1(x1::T, y1::T) where {T<:Real} = @inbounds integrate_avx(z -> f(x1, y1, z), xmin[3], xmax[3], x, w, h)
+    f2(x1::T) where {T<:Real} = @inbounds integrate(y -> f1(x1, y), xmin[2], xmax[2], x, w, h)
+    return @inbounds integrate(x1 -> f2(x1), xmin[1], xmax[1], x, w, h)
 end
 
 # convenience function to convert AbsrtactVectors to SVectors
@@ -149,25 +211,25 @@ function _integrate(f::Function, D::Int, x::AbstractVector{T}, w::AbstractVector
 end
 
 # helper function for generality
-function quad(f::Function, xmin::T, xmax::T, x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real}
+@inline function quad(f::Function, xmin::T, xmax::T, x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real}
     if xmin == xmax
         return zero(T)
     end
 
     #ncalls = @MVector [0]
     if xmin > xmax
-        return -integrate(f, xmax, xmin, x, w, h)#, ncalls[1]
+        return -integrate_avx(f, xmin, xmax, x, w, h)#, ncalls[1]
     end
 
     if xmin == -one(T) && xmax == one(T)
-        return integrate(f, x, w, h)#, ncalls[1]
+        return integrate_avx(f, x, w, h)#, ncalls[1]
     else
-        return integrate(f, xmin, xmax, x, w, h)#, ncalls[1]
+        return integrate_avx(f, xmin, xmax, x, w, h)#, ncalls[1]
     end
 end
 
 # 2D
-function quad(f::Function, xmin::SVector{2,T}, xmax::SVector{2,T},
+@inline function quad(f::Function, xmin::SVector{2,T}, xmax::SVector{2,T},
     x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real}
     if (xmin[1] == xmax[1]) || (xmin[2] == xmax[2])
         return zero(T)
@@ -182,7 +244,7 @@ function quad(f::Function, xmin::SVector{2,T}, xmax::SVector{2,T},
 end
 
 # 3D
-function quad(f::S, xmin::SVector{3,T}, xmax::SVector{3,T},
+@inline function quad(f::S, xmin::SVector{3,T}, xmax::SVector{3,T},
     x::AbstractVector{T}, w::AbstractVector{T}, h::T) where {T<:Real,S}
     if (xmin[1] == xmax[1]) || (xmin[2] == xmax[2]) || (xmin[3] == xmax[3])
         return zero(T)
